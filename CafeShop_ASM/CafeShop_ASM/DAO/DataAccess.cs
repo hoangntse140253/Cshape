2e441bd2237f365a62a7e1d9fc7d2cf93391f285
hoangntse140253 <80318057+hoangntse140253@users.noreply.github.com>
2021-03-27 08:01:49 +0700

    first commit

---
+++ CafeShop_ASM/CafeShop_ASM/DAO/DataAccess.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CafeShop_ASM.DAO
+{
+    public class DataAccess
+    {
+        private string connectionStr = @"server=ADMIN;database=CafeShop_Managerment;uid=sa;pwd=TH0402";
+
+        private static DataAccess instance;
+        
+        public static DataAccess Instance {
+            get { if (instance == null) instance = new DataAccess(); return DataAccess.instance; }
+            private set { DataAccess.instance = value; }
+        }
+        private DataAccess() { }
+
+        public DataTable ExecuteQuery(string sql)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionStr)){
+
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                try
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    adapter.Fill(data);
+                }
+                catch (SqlException e)
+                {
+                    throw new Exception(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return data;
+        }
+        public int ExecuteNonQuery(string sql)
+        {
+            int data = 0;
+
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                SqlCommand command = new SqlCommand(sql, connection);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+                try
+                {
+                    if (connection.State == ConnectionState.Closed)
+                    {
+                        connection.Open();
+                    }
+                    data = command.ExecuteNonQuery();
+                }
+                catch (SqlException e)
+                {
+                    throw new Exception(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return data;
+        }
+    }
+}
+
